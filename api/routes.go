907ee208d7610d49a7ce60fee7a04c19fbc219e6
Thalisonh <thalison_henrique@hotmail.com>
2022-06-26 23:53:32 -0300

    add books to routes

--- api/routes.go
+++ api/routes.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"github.com/Thalisonh/go-medium/api/books"
 	"github.com/Thalisonh/go-medium/api/health"
 	"github.com/gin-gonic/gin"
 )
@@ -9,4 +10,5 @@ func Router(r *gin.Engine) {
 	req := r.Group("/api")
 
 	health.Router(req.Group("/health"))
+	books.Router(req.Group("/books"))
 }
